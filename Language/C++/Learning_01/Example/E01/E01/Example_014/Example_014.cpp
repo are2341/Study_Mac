3d0542452f5e5bb364475734fe355fdd2ac8031f
dante <are2341@nate.com>
2021-02-14 13:08:57 +0900

    no message

---
+++ Language/C++/Learning_01/Example/E01/E01/Example_014/Example_014.cpp
@@ -0,0 +1,15 @@
+//
+//  Example_014.cpp
+//  E01
+//
+//  Created by 이상동 on 2021/02/14.
+//
+
+#include "Example_014.hpp"
+
+namespace EXAMPLE_014 {
+	//! Example 14
+	void Example_014(const int argc, const char **args) {
+		std::cout << "Hello, World!" << std::endl;
+	}
+}
---
+++ Language/C++/Learning_01/Example/E02/E02/Example_029/Example_029.hpp
@@ -0,0 +1,18 @@
+//
+//  Example_029.hpp
+//  E02
+//
+//  Created by 이상동 on 2021/02/14.
+//
+
+#ifndef Example_029_hpp
+#define Example_029_hpp
+
+#include "../Global/Define/KGDefine.hpp"
+
+namespace EXAMPLE_029 {
+	//! Example 29
+	void Example_029(const int argc, const char **args);
+}
+
+#endif /* Example_029_hpp */
