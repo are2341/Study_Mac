bf0d330c93161735c06321129f52183eda79dc7d
dante <are2341@nate.com>
2021-02-15 20:08:51 +0900

    no message

---
+++ Engine/Cocos2d-x/2.x/Learning_01/Example/Classes/Global/Define/KGDefine.hpp
@@ -0,0 +1,19 @@
+//
+//  KGDefine.hpp
+//  Example-mobile
+//
+//  Created by 이상동 on 2021/02/15.
+//
+
+#ifndef KGDefine_hpp
+#define KGDefine_hpp
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+#include "SimpleAudioEngine.h"
+
+using namespace cocos2d;
+using namespace cocos2d::ui;
+using namespace CocosDenshion;
+
+#endif /* KGDefine_hpp */
