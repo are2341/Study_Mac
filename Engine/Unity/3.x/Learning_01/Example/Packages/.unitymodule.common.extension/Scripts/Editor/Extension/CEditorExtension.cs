aeda040809a53d6686fd74046dd25f41dda591d7
dante <are2341@nate.com>
2021-02-15 21:50:39 +0900

    no message

---
+++ Engine/Unity/3.x/Learning_01/Example/Packages/.unitymodule.common.extension/Scripts/Editor/Extension/CEditorExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+//! 에디터 기본 접근 확장
+public static partial class CEditorExtension {
+    #region 클래스 함수
+	//! 직렬화 속성 값을 변경한다
+	public static void ExSetPropertyValue(this SerializedObject a_oSender, string a_oName, System.Action<SerializedProperty> a_oCallback) {
+		CAccess.Assert(a_oSender != null && a_oName.ExIsValid());
+		
+		var oProperty = a_oSender.FindProperty(a_oName);
+		a_oCallback?.Invoke(oProperty);
+
+		a_oSender.ApplyModifiedProperties();
+		a_oSender.Update();
+	}
+	#endregion			// 클래스 함수
+}
+#endif			// #if UNITY_EDITOR
---
+++ Engine/Unity/3.x/Learning_01/Example/Packages/.unitymodule.common.extension/Scripts/Runtime/Extension/CExtension+Setup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//! 설정 확장 클래스
+public static partial class CExtension {
+	#region 클래스 함수
+	//! 2 차원 카메라를 설정한다
+	public static void ExSetup2D(this Camera a_oSender, float a_fPlaneHeight) {
+		CAccess.Assert(a_oSender != null && a_fPlaneHeight.ExIsGreate(KCDefine.B_VALUE_FLT_0));
+		
+		a_oSender.orthographic = true;
+		a_oSender.orthographicSize = a_fPlaneHeight / 2.0f;
+	}
+
+	//! 3 차원 카메라를 설정한다
+	public static void ExSetup3D(this Camera a_oSender, float a_fPlaneHeight, float a_fPlaneDistance) {
+		CAccess.Assert(a_oSender != null);
+		CAccess.Assert(a_fPlaneHeight.ExIsGreate(KCDefine.B_VALUE_FLT_0) && a_fPlaneDistance.ExIsGreate(KCDefine.B_VALUE_FLT_0));
+
+		float fFOV = Mathf.Atan((a_fPlaneHeight / 2.0f) / a_fPlaneDistance);
+
+		a_oSender.orthographic = false;
+		a_oSender.fieldOfView = (fFOV * 2.0f) * Mathf.Rad2Deg;
+	}
+	#endregion			// 클래스 함수
+}
