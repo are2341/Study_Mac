d5241fcba28fc26acbd40203a103342d88c19ecc
dante <are2341@nate.com>
2021-04-12 01:30:49 +0900

    no message

---
+++ Framework/Algorithm/C/Learning_01/Example/E01/E01/Global/Define/KGDefine.hpp
@@ -0,0 +1,17 @@
+//
+//  KGDefine.hpp
+//  E01
+//
+//  Created by 이상동 on 2021/04/12.
+//
+
+#ifndef KGDefine_hpp
+#define KGDefine_hpp
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#endif /* KGDefine_hpp */
---
+++ Framework/Algorithm/C/Learning_01/Example/E01/E01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  E01
+//
+//  Created by 이상동 on 2021/04/12.
+//
+
+#include "Example_058/Example_058.hpp"
+
+//! 메인 함수
+int main(const int argc, const char **args) {
+	E058::Example_058(argc, args);
+	return 0;
+}
