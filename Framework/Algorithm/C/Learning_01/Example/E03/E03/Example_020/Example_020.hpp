a134af0cfe0dc36efe6c59b6a507067c9a767fc3
dante <are2341@nate.com>
2021-05-19 01:59:56 +0900

    no message

---
+++ Framework/Algorithm/C/Learning_01/Example/E03/E03/Example_020/Example_020.hpp
@@ -0,0 +1,18 @@
+//
+//  Example_020.hpp
+//  E03
+//
+//  Created by 이상동 on 2021/05/19.
+//
+
+#ifndef Example_020_hpp
+#define Example_020_hpp
+
+#include "../Global/Define/KGDefine.hpp"
+
+namespace E020 {
+	//! Example 20
+	void Example_020(const int argc, const char **args);
+}
+
+#endif /* Example_020_hpp */
