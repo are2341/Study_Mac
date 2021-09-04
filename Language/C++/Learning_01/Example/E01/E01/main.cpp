a1762ec2d8d6e54ea59c5434f1b544563b095dc1
dante <are2341@nate.com>
2021-09-05 00:59:09 +0900

    no message

--- Language/C++/Learning_01/Example/E01/E01/main.cpp
+++ Language/C++/Learning_01/Example/E01/E01/main.cpp
@@ -5,9 +5,11 @@
 //  Created by 이상동 on 2021/09/04.
 //
 
-#include <iostream>
+#include "Example_014/Example_014.hpp"
 
 //! 메인 함수
 int main(const int argc, const char **args) {
+	E014::Example_014(argc, args);
+	
 	return 0;
 }
