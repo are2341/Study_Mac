6a265caee70b7ff2e022b364c0cf032e111e1037
dante <are2341@nate.com>
2021-11-21 00:35:43 +0900

    no message

--- Framework/Algorithm/Python/Example/E01/Example.py
+++ Framework/Algorithm/Python/Example/E01/Example.py
@@ -7,7 +7,7 @@ from Example_026.Example_026 import Example_026
 
 # 메인 모듈 일 경우
 if __name__ == "__main__":
-	# Example_017(sys.argv)
-	Example_021(sys.argv)
+	Example_017(sys.argv)
+	# Example_021(sys.argv)
 	# Example_026(sys.argv)
 	
