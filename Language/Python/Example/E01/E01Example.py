49c19ab8b713d5cfda311ed64fe4b741f27906eb
sd.lee <sd.lee@ninetap.com>
2022-06-01 17:34:44 +0900

    no message

--- Language/Python/Example/E01/E01Example.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-from Example_036.Example_036 import Example_036
-
-# 메인 모듈 일 경우
-if __name__ == "__main__":
-	Example_036(sys.argv)
-	
--- Language/Python/Example/E02/E02Example.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# 메인 모듈 일 경우
-if __name__ == "__main__":
-	print("Hello, World!")
-	
