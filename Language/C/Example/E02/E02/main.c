6cf1e91cd8a2440430851ebc385437dab49587ee
sd.lee <sd.lee@ninetap.com>
2022-06-01 17:23:12 +0900

    no message

---
+++ Language/C/Example/E02/E02/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  E02
+//
+//  Created by 이상동 on 2022/06/01.
+//
+
+#include <stdio.h>
+
+/** 메인 함수 */
+int main(const int argc, const char **args) {
+	printf("Hello, World!\n");
+	return 0;
+}
--- Language/Python/Example/E02/E02Example.py
+++ Language/Python/Example/E02/E02Example.py
@@ -1,9 +1,7 @@
 import os
 import sys
 
-from Example_036.Example_036 import Example_036
-
 # 메인 모듈 일 경우
 if __name__ == "__main__":
-	Example_036(sys.argv)
+	print("Hello, World!")
 	
