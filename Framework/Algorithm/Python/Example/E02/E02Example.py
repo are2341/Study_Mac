74f27db35ddbd2b47ca37f9b4294370e246d370f
sd.lee <sd.lee@ninetap.com>
2022-06-03 07:20:48 +0900

    no message

---
+++ Framework/Algorithm/Python/Example/E02/E02Example.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+from Example_019.Example_019 import Example_019
+
+# 메인 모듈 일 경우
+if __name__ == "__main__":
+	Example_019(sys.argv)
+	
---
+++ Framework/Algorithm/Python/Example/E02/Example_019/Example_019.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# Example 19
+def Example_019(args:[str]):
+	print("Hello, World!")
+	
