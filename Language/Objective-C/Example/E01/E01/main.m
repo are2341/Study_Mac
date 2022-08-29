0d838ad0e90eb794aa06369ca84c3fe2400b7471
sd.lee <sd.lee@ninetap.com>
2022-08-29 20:13:41 +0900

    no message

---
+++ Language/Objective-C/Example/E01/E01/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  E01
+//
+//  Created by 이상동 on 2022/08/29.
+//
+
+#import <Foundation/Foundation.h>
+
+/** 메인 함수 */
+int main(const int argc, const char **args) {
+	NSLog(@"Hello, World!");
+	return 0;
+}
